9c3f8e2844cff57f7c9c2db27e9414567adb62fe
Thalia Lira <thaliasouza_lira@hotmail.com>
2024-05-16 22:12:38 -0300

    Esqueleto do projeto

---
+++ src/main/java/com/example/transferencias/controller/CadastroClienteController.java
@@ -0,0 +1,14 @@
+package com.example.transferencias.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/cadastrocliente")
+
+public class CadastroClienteController {
+
+
+
+}
